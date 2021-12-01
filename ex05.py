3275610b67aa1d6ef819d6011ee643802e9a577d
rafidini <itokiana.rafidinarivo@edu.esiee.fr>
2021-12-01 14:17:45 +0100

    ex5

--- ex05.py
+++ ex05.py
@@ -1,4 +1,10 @@
 
+def manhattan_distance(pos1, pos2):
+  return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+def euclidian_distance(pos1, pos2):
+  return (pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2
+
 def corners_heuristic(state, problem):
     """
     A heuristic for the CornersProblem that you defined.
@@ -10,17 +16,21 @@ def corners_heuristic(state, problem):
     This function should always return a number that is a lower bound on the
     shortest path from the state to a goal of the problem; i.e.  it should be
     admissible (as well as consistent).
+
+    euclidian_distance: 1445
+    manhattan_distance: 1690
     """
     corners = problem.corners  # These are the corner coordinates
     walls = problem.walls  # These are the walls of the maze, as a Grid (game.py)
+    current_position = state[0]
 
     # TODO Compute distances - current position to corners
-    distances = [0, 0]
+    distances = [manhattan_distance(current_position, corner) for corner in corners]
+    
+    # Extract min distance
+    min_distance = min(distances)
 
-    # Extract max distance
-    max_heuristic = max(distances)
-
-    return max_heuristic
+    return min_distance
 
 
 if __name__ == '__main__':
